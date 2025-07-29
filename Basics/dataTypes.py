9b151c230c6a48fdd0aba919785f887e3cfd1f93
pragesh-yadav <drop2pragesh@gmail.com>
2025-07-29 14:17:30 +0530

    Data Types

---
+++ Basics/dataTypes.py
@@ -0,0 +1,62 @@
+# Data Types in Python
+
+# Integer - whole number negative to positive 
+# Float  - Number with decimal Value
+#  String
+# None
+
+# Integers
+num  = 25
+print(num)
+
+# type
+a = 7
+print(type(a)) # Type of a is Integer class 
+# class is having void number of object having similar kind of property
+
+print(a)
+
+# Float
+# Having number with decimal
+
+f = 3.5
+print(f)
+print(type(f))
+
+# Booleans
+#  Use for confermation 
+
+b = True
+print(type(b))
+
+b1 = False
+print(type(b1)) # class boolean
+
+# Strings
+# Use single, double quotes
+s = "Pragesh 24 @"
+print(type(s))
+print(s)
+
+st = 'Data store in 3 +'
+print(type(st))
+print(st)
+
+sr = """
+This is a multi line string !
+"""
+
+print(sr)
+print(type(sr)) # return class 'str'
+
+
+# None - No value
+
+n = None
+
+print(type(n)) #return class NoneType
+
+print(n)
+
+
+
