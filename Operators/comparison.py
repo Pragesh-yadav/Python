cf8703b6a8ee550ab0c3b5822cba10e06d4951f2
pragesh-yadav <drop2pragesh@gmail.com>
2025-07-31 23:03:48 +0530

    Comparison operator less than, grater than, equal to, not Equal to etc

---
+++ Operators/comparison.py
@@ -0,0 +1,32 @@
+""""
+    Comparison 
+    ==
+    !=
+    <
+    >
+    <=
+    >=
+"""
+
+
+#  Comparison operators can be used to compre values in mathematical terms.
+
+a = 8
+b = 34
+
+print(a == b) # return false
+
+print(3 == 3) # return true
+
+print(3 != 3) #return false
+
+print(a > b) # return flase
+
+print(a < b) # return true
+
+print(a <= b) # return true either of the condition true it will return true
+
+print(6 >= 9) # return flase because both condition faild
+
+
+print(9 >= 3) # return true
