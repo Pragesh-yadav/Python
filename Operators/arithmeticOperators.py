be6a0e35981fc5f318628ce8e1e1caa71723a303
pragesh-yadav <drop2pragesh@gmail.com>
2025-07-31 23:01:24 +0530

    Arithmetic operators loke Modulo, to the power, add, subtract, floor division etc

---
+++ Operators/arithmeticOperators.py
@@ -0,0 +1,61 @@
+"""
+    . Addition
+    . Subtraction
+    . Division
+    . Modulo %
+    . Multiplication
+    . Floor division(//)
+    . to the power(**)
+    
+    # These operators allow us to perform arithmetic operations in Python
+
+"""
+#  Addition
+a = 5
+b =3
+
+print(a + b) # return 8
+
+# Subtraction
+
+print(a - b) # return 2
+
+# Division 
+print(a / b) # return 1.666666666666
+
+#  Divisin operator will always yield float value
+
+# Multiplication
+
+print(a * b) # return 15
+
+firstName = "Jones"
+lastName = "Chals"
+
+#  String Concatenation
+
+print(firstName + lastName) # return Jones Chals
+
+print("1" + "2") # return 12 because of string concatenation
+
+#  Multiply string with Number
+
+print(firstName * 3) # return JonesJonesJones
+#  But we can't multiply string into string
+
+# Module % operator
+
+print(a % b) # return  remainder 2
+
+# In floor division, the result is floored to the nearest left side of the smaller integer
+
+print(5 // 3) # return 1
+
+#  To the power accepts two values base and power
+
+print(a ** b) # return 125
+
+print(2 ** 3) # return 8 
+
+
+
