d55d99d429cce18329aa0b997fea2d2b76bdf22d
pragesh-yadav <drop2pragesh@gmail.com>
2025-07-31 22:35:38 +0530

    Print function with using inbuild method like sep, end

---
+++ Basics/printFunction.py
@@ -0,0 +1,19 @@
+# Printing multiple values using print function
+
+print("Pragesh")
+
+
+print("Name", "age", "DOB", "Place", "MisileMan", "34", 1956, "India") # printing multiple value
+
+
+# Using Print funtion print has three inbuild method value, sep=' ', end='\n'
+#  We can change this according to our need
+# print? to show the method of print function
+# Changing from by default to user value
+
+print("Name", "age", sep="\n")
+
+print("Ana", 23, end = '=>')
+
+print("City", "State", "Country", sep=',', end=' ') #Seprate with , and end with whitespace
+
