72b691ff82fa347f1384531b3cba820d248c9373
pragesh-yadav <drop2pragesh@gmail.com>
2025-07-31 23:54:18 +0530

    Assignment declaration in modern way

---
+++ Operators/assignmentOp.py
@@ -0,0 +1,25 @@
+"""
+    Assignment Operators
+    =
+    -=
+    +=
+    /=
+    *=
+"""
+
+a =  4 # assigning the value of a 
+
+a = a + 4
+# a += 4 is same as a =  a + 4
+print(a) # return 8
+
+a -= 4
+print(a) # return 4
+
+a /= 4
+
+print(a) # return 1
+
+a *= 4
+print(a) # return 4
+
