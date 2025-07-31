4d0aa75e9622d0e8b95a975c872a05237fdc90f0
pragesh-yadav <drop2pragesh@gmail.com>
2025-07-31 22:37:09 +0530

    Taking input from users and conver it in to float, int.

---
+++ Basics/inputFunction.py
@@ -0,0 +1,31 @@
+# Input using input function
+name = input() # Using for take String input from user
+
+print(type(name)) # class string
+
+# Number as input
+
+x = input() # x = 4
+
+print(x) # 4
+
+print(type(x)) # by default input function type is string
+
+
+#  Type Conversion
+
+y = float(x) 
+
+print(y) # 4.0
+
+print(type(y)) # class float
+
+b = int(x) # Convert string to int
+
+#  But we can't conver string like "Name" it will through error
+
+
+
+
+
+
