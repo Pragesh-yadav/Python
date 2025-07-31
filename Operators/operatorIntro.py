94667e9a43e7600b0b70fa89dfc59345ea48f6f8
pragesh-yadav <drop2pragesh@gmail.com>
2025-07-31 22:42:44 +0530

    Intro of Operators

---
+++ Operators/operatorIntro.py
@@ -0,0 +1,10 @@
+"""
+    Expression
+        . Operators
+        . Operands
+            . Operators + Operands = Expression
+            
+        (x + y) this is expression 
+        x and y is operand
+        + is operator
+"""
