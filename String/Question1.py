ede6f97f11a1fb05b5f0ef97e98818c40aae435e
pragesh-yadav <drop2pragesh@gmail.com>
2025-09-22 17:30:34 +0530

    Print all vowels of a given string

---
+++ String/Question1.py
@@ -0,0 +1,7 @@
+# Print all vowels of a given string
+
+text = "The quick brown fox jumps over the lazy dog"
+
+for i in text:
+    if i=='a' or i=='e' or i=='i' or i=='o' or i=='u':
+        print(i, end=" ")
